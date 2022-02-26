7d1ce7521f8789eb118de812d9bcf660ac7bc001
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-26 09:42:31 -0400

    refactor: Input Component
    Change class component to function component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,17 +3,26 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
+
 } from 'react';
+
+import { IconBaseProps } from 'react-icons';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+export function Input({name, icon: Icon,...rest}: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState<Boolean>(false);
+  const [isFilled, setIsFilled] = useState<Boolean>(false);
 
   const { fieldName, defaultValue, registerField } = useField(name);
 
@@ -26,14 +35,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
     setIsFilled(!!inputRef.current?.value);
   }, []);
-
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: inputRef.current,
-      path: 'value',
-    });
-  }, [fieldName, registerField]);
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
@@ -48,6 +49,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
